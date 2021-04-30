9dcf494057ff446840f570b5667dbf56dc30766b
dl7476 <83216773+dl7476@users.noreply.github.com>
2021-04-30 19:53:50 +0200

    Create boot.py

---
+++ Micropython/boot.py
@@ -0,0 +1,36 @@
+import network
+import machine
+import utime
+
+machine.freq(240000000)
+
+
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+
+#access point
+ssid = 'ESP'
+password = '4037'
+
+
+
+#access point
+station = network.WLAN(network.AP_IF)
+
+station.active(True)
+
+station.config(essid=ssid,password=password)
+
+while station.active() == False:
+    pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+
+
